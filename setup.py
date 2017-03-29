585a2e72183ed378d7a591bd3dbf87305241c4eb
Ryan Dwyer <ryanpdwyer@gmail.com>
2017-03-29 16:15:31 -0400

    Add sigutils

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     # add to docs/conf.py MAGIC_MOCK and .travis.yml 'conda install ...'
     # instead of here
     # For example, numpy, scipy, matplotlib, pandas are there instead
-    install_requires=["decorator", "six"],
+    install_requires=["decorator", "six", "sigutils"],
 
     tests_require=['nose'],
     test_suite='nose.collector',
